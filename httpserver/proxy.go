477b02c8a343e1ca081ea231e499f84f54b1e859
Nanxi <leenanxi@gmail.com>
2020-12-05 01:56:41 +0800

    Create proxy.go

---
+++ httpserver/proxy.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/http/httpguts"
+	"golang.org/x/net/http2"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"time"
+)
+
+// StatusClientClosedRequest non-standard HTTP status code for client disconnection.
+const StatusClientClosedRequest = 499
+
+// StatusClientClosedRequestText non-standard HTTP status for client disconnection.
+const StatusClientClosedRequestText = "Client Closed Request"
+
+
+var proxy http.Handler
+var defaultRoundTripper http.RoundTripper
+
+func init()  {
+	var err error
+	defaultRoundTripper, err = createRoundtripper()
+	if err != nil {
+		panic(err)
+	}
+	pool := newBufferPool()
+	proxy, err = buildProxy(false, defaultRoundTripper, pool)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func buildProxy(passHostHeader bool, defaultRoundTripper http.RoundTripper, bufferPool httputil.BufferPool) (http.Handler, error) {
+	flushInterval := 100 * time.Millisecond
+	proxy := &httputil.ReverseProxy{
+		Director: func(outReq *http.Request) {
+			u := outReq.URL
+			if outReq.RequestURI != "" {
+				parsedURL, err := url.ParseRequestURI(outReq.RequestURI)
+				if err == nil {
+					u = parsedURL
+				}
+			}
+
+			outReq.URL.Path = u.Path
+			outReq.URL.RawPath = u.RawPath
+			outReq.URL.RawQuery = u.RawQuery
+			outReq.RequestURI = "" // Outgoing request should not have RequestURI
+
+			outReq.Proto = "HTTP/1.1"
+			outReq.ProtoMajor = 1
+			outReq.ProtoMinor = 1
+
+			if _, ok := outReq.Header["User-Agent"]; !ok {
+				outReq.Header.Set("User-Agent", "")
+			}
+
+			// Do not pass client Host header unless optsetter PassHostHeader is set.
+			if !passHostHeader {
+				outReq.Host = outReq.URL.Host
+			}
+
+			// Even if the websocket RFC says that headers should be case-insensitive,
+			// some servers need Sec-WebSocket-Key, Sec-WebSocket-Extensions, Sec-WebSocket-Accept,
+			// Sec-WebSocket-Protocol and Sec-WebSocket-Version to be case-sensitive.
+			// https://tools.ietf.org/html/rfc6455#page-20
+			outReq.Header["Sec-WebSocket-Key"] = outReq.Header["Sec-Websocket-Key"]
+			outReq.Header["Sec-WebSocket-Extensions"] = outReq.Header["Sec-Websocket-Extensions"]
+			outReq.Header["Sec-WebSocket-Accept"] = outReq.Header["Sec-Websocket-Accept"]
+			outReq.Header["Sec-WebSocket-Protocol"] = outReq.Header["Sec-Websocket-Protocol"]
+			outReq.Header["Sec-WebSocket-Version"] = outReq.Header["Sec-Websocket-Version"]
+			delete(outReq.Header, "Sec-Websocket-Key")
+			delete(outReq.Header, "Sec-Websocket-Extensions")
+			delete(outReq.Header, "Sec-Websocket-Accept")
+			delete(outReq.Header, "Sec-Websocket-Protocol")
+			delete(outReq.Header, "Sec-Websocket-Version")
+		},
+		Transport:     defaultRoundTripper,
+		FlushInterval: flushInterval,
+		BufferPool:    bufferPool,
+		ErrorHandler: func(w http.ResponseWriter, request *http.Request, err error) {
+			statusCode := http.StatusInternalServerError
+
+			switch {
+			case errors.Is(err, io.EOF):
+				statusCode = http.StatusBadGateway
+			case errors.Is(err, context.Canceled):
+				statusCode = StatusClientClosedRequest
+			default:
+				var netErr net.Error
+				if errors.As(err, &netErr) {
+					if netErr.Timeout() {
+						statusCode = http.StatusGatewayTimeout
+					} else {
+						statusCode = http.StatusBadGateway
+					}
+				}
+			}
+			log.Debugf("'%d %s' caused by: %v", statusCode, statusText(statusCode), err)
+			w.WriteHeader(statusCode)
+			_, werr := w.Write([]byte(statusText(statusCode)))
+			if werr != nil {
+				log.Debugf("Error while writing status code", werr)
+			}
+		},
+	}
+
+	return proxy, nil
+}
+
+func statusText(statusCode int) string {
+	if statusCode == StatusClientClosedRequest {
+		return StatusClientClosedRequestText
+	}
+	return http.StatusText(statusCode)
+}
+
+
+
+// createRoundtripper creates an http.Roundtripper configured with the Transport configuration settings.
+// For the settings that can't be configured in Traefik it uses the default http.Transport settings.
+// An exception to this is the MaxIdleConns setting as we only provide the option MaxIdleConnsPerHost
+// in Traefik at this point in time. Setting this value to the default of 100 could lead to confusing
+// behavior and backwards compatibility issues.
+func createRoundtripper() (http.RoundTripper, error) {
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+		DualStack: true,
+	}
+	transport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialer.DialContext,
+		MaxIdleConnsPerHost:   200,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	transport.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	smartTransport, err := newSmartRoundTripper(transport)
+	if err != nil {
+		return nil, err
+	}
+
+	return smartTransport, nil
+}
+
+
+func newSmartRoundTripper(transport *http.Transport) (http.RoundTripper, error) {
+	transportHTTP1 := transport.Clone()
+
+	err := http2.ConfigureTransport(transport)
+	if err != nil {
+		return nil, err
+	}
+
+	return &smartRoundTripper{
+		http2: transport,
+		http:  transportHTTP1,
+	}, nil
+}
+
+type smartRoundTripper struct {
+	http2 *http.Transport
+	http  *http.Transport
+}
+
+// smartRoundTripper implements RoundTrip while making sure that HTTP/2 is not used
+// with protocols that start with a Connection Upgrade, such as SPDY or Websocket.
+func (m *smartRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	// If we have a connection upgrade, we don't use HTTP/2
+	if httpguts.HeaderValuesContainsToken(req.Header["Connection"], "Upgrade") {
+		return m.http.RoundTrip(req)
+	}
+	return m.http2.RoundTrip(req)
+}
+
+
+
+const bufferPoolSize = 32 * 1024
+
+func newBufferPool() *bufferPool {
+	return &bufferPool{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return make([]byte, bufferPoolSize)
+			},
+		},
+	}
+}
+
+type bufferPool struct {
+	pool sync.Pool
+}
+
+func (b *bufferPool) Get() []byte {
+	return b.pool.Get().([]byte)
+}
+
+func (b *bufferPool) Put(bytes []byte) {
+	b.pool.Put(bytes)
+}
