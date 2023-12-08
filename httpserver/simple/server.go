6ac53a6a4be08f6891f5bf8184926a10a42fca57
Nanxi <leenanxi@gmail.com>
2023-12-08 17:51:31 +0800

    http/3 first

--- httpserver/simple/server.go
+++ httpserver/simple/server.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -14,25 +17,29 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/quic-go/quic-go/http3"
+	"github.com/rs/zerolog/log"
 )
 
 var (
 	port       = flag.Int("p", 8080, "port to listen")
 	decodeBody = flag.Bool("b", false, "decode as struct")
+	cors       = flag.Bool("cors", true, "enable cors?")
 	dir        = flag.String("d", "./", "dir to server")
-	spa        = flag.Bool("spa", true, "is is a single page app?")
+	spa        = flag.Bool("spa", true, "is a single page app?")
 	mode       = flag.String("mode", "file", "default router mode [file, info, proxy]")
-	host       = flag.String("host", "", "ssl cert")
+	host       = flag.String("host", "", "the host")
+	cert       = flag.String("cert", "", "ssl cert")
+	key        = flag.String("key", "", "ssl key")
+
+	udp = flag.Bool("udp", false, "udp only")
 )
 
 func main() {
 	flag.Parse()
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		fmt.Printf("\u001b[31m[ERROR] %s\u001b[0m\n", err)
-		return
-	}
 	fs := fileServer(*dir)
+
 	http.Handle("/_info/", StripPrefix("/_info", http.HandlerFunc(httpInfo)))
 	http.Handle("/_www/", StripPrefix("/_www", fs))
 	switch *mode {
@@ -43,8 +50,17 @@ func main() {
 	default:
 		http.Handle("/", fs)
 	}
-	fmt.Printf("Starting up http-server, serving \u001b[36m%s \u001b[0m \nAvailable on:\n", *dir)
+	handler := LogHandler(http.DefaultServeMux)
+
 	scheme := "http"
+	server := "http-server"
+	if *cert != "" && *key != "" {
+		scheme = "https"
+		if *udp {
+			server = "http3-server"
+		}
+	}
+	fmt.Printf("Starting up %s serving \u001b[36m%s \u001b[0m \nAvailable on:\n", server, *dir)
 	if *host == "" {
 		addrs, _ := net.InterfaceAddrs()
 		for _, rawAddr := range addrs {
@@ -52,12 +68,27 @@ func main() {
 				fmt.Printf("\t\u001b[34m%s://%s:%d\u001b[0m\n", scheme, ipnet.IP.To4().String(), *port)
 			}
 		}
+		ipV6, err := getIPV6()
+		if err == nil {
+			fmt.Printf("\t\u001b[34m%s://[%s]:%d\u001b[0m\n", scheme, ipV6, *port)
+		}
 	} else {
 		fmt.Printf("\t\u001b[34m%s://%s:%d\u001b[0m\n", scheme, *host, *port)
 	}
 	fmt.Println("Hit CTRL-C to stop the server")
-	panic(http.Serve(listener, nil))
+
+	addr := fmt.Sprintf(":%d", *port)
+	var err error
+	if scheme == "https" {
+		err = ListenAndServeHTTP3(addr, *cert, *key, handler, *udp)
+	} else {
+		err = http.ListenAndServe(fmt.Sprintf(":%d", *port), handler)
+	}
+	if err != nil {
+		fmt.Printf("\u001b[31m[HTTP3 ERROR] %s\u001b[0m\n", err)
+	}
 }
+
 func fileServer(dir string) http.Handler {
 	absPath, err := filepath.Abs(dir)
 	if err != nil {
@@ -88,6 +119,7 @@ func fileServer(dir string) http.Handler {
 	})
 }
 
+// URL the url
 type URL struct {
 	Scheme   string
 	Path     string
@@ -148,7 +180,7 @@ func getRequestInfo(r *http.Request) *request {
 		}
 	}
 	req := request{Time: time.Now().Format(time.RFC3339)}
-	var meta = make(map[string]string)
+	meta := make(map[string]string)
 	req.Host = r.Host
 	req.Method = r.Method
 	req.URL = u
@@ -159,35 +191,31 @@ func getRequestInfo(r *http.Request) *request {
 	req.Proto = r.Proto
 	req.ContentLength = r.ContentLength
 	contentType := r.Header.Get("Content-Type")
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	r.Body = ioutil.NopCloser(bytes.NewReader(reqBody))
-	if !strings.HasPrefix(contentType, "application/grpc") {
-		if *decodeBody {
-			if strings.Contains(contentType, "json") {
-				var data interface{}
-				dec := json.NewDecoder(r.Body)
-				if err := dec.Decode(&data); err == nil {
-					req.Body = data
-				} else {
-					meta["error"] = fmt.Sprintf("json decode error - %s", err)
-				}
-			} else if strings.Contains(contentType, "form") {
-				r.ParseForm()
-				req.Body = r.PostForm
-			} else if strings.Contains(contentType, "grpc") {
-				//do noting
+	reqBody, _ := io.ReadAll(r.Body)
+	r.Body = io.NopCloser(bytes.NewReader(reqBody))
+	if *decodeBody {
+		if strings.Contains(contentType, "json") {
+			var data interface{}
+			dec := json.NewDecoder(r.Body)
+			if err := dec.Decode(&data); err == nil {
+				req.Body = data
 			} else {
-				if r.Method == "GET" || r.Method == "DELETE" {
-					req.Body = ""
-				} else {
-					if len(reqBody) > 0 {
-						req.Body = string(reqBody)
-					}
-				}
+				meta["error"] = fmt.Sprintf("json decode error - %s", err)
 			}
+		} else if strings.Contains(contentType, "form") {
+			r.ParseForm()
+			req.Body = r.PostForm
+		} else if strings.Contains(contentType, "grpc") {
+			// do noting
 		} else {
-			req.Body = string(reqBody)
+			if r.Method == "GET" || r.Method == "DELETE" {
+				req.Body = ""
+			} else if len(reqBody) > 0 {
+				req.Body = string(reqBody)
+			}
 		}
+	} else {
+		req.Body = string(reqBody)
 	}
 	req.RemoteIP = getIP(r).String()
 	if len(meta) > 0 {
@@ -202,10 +230,11 @@ func httpInfo(w http.ResponseWriter, r *http.Request) {
 
 	if strings.HasSuffix(r.URL.Path, ".html") {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, "<html><head><title>%s</title><head/><h1>%s</h1><pre>%s</pre></html>", "Request Infos", "Request Infos", string(resp))
+		_, _ = fmt.Fprintf(w, "<html><head><title>%s</title><head/><h1>%s</h1>"+
+			"<pre>%s</pre></html>", "Request Infos", "Request Infos", string(resp))
 	} else {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(resp)
+		_, _ = w.Write(resp)
 	}
 }
 
@@ -249,4 +278,108 @@ func StripPrefix(prefix string, h http.Handler) http.Handler {
 			http.NotFound(w, r)
 		}
 	})
+}
+
+func getIPV6() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", fmt.Errorf("interfaces error %w", err)
+	}
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagLoopback == 0 && iface.Flags&net.IPv6len != 0 {
+			addrs, err := iface.Addrs()
+			if err != nil {
+				return "", fmt.Errorf("addr error %w", err)
+			}
+			for _, addr := range addrs {
+				ipNet, ok := addr.(*net.IPNet)
+				if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() == nil {
+					if !(strings.HasPrefix(ipNet.IP.String(), "fe80:") || strings.HasPrefix(ipNet.IP.String(), "fc00:")) {
+						return ipNet.IP.String(), nil
+					}
+				}
+			}
+		}
+	}
+	return "", errors.New("ip not found")
+}
+
+// ListenAndServeHTTP3 listens on the given network address for both, TLS and QUIC
+func ListenAndServeHTTP3(addr, certFile, keyFile string, handler http.Handler, http3Only bool) error {
+	// Load certs
+	var err error
+	certs := make([]tls.Certificate, 1)
+	certs[0], err = tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return err
+	}
+	// We currently only use the cert-related stuff from tls.Config,
+	// so we don't need to make a full copy.
+	config := &tls.Config{
+		Certificates: certs,
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	if addr == "" {
+		addr = ":https"
+	}
+
+	// Open the listeners
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return err
+	}
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return err
+	}
+	defer udpConn.Close()
+
+	if handler == nil {
+		handler = http.DefaultServeMux
+	}
+	// Start the servers
+	quicServer := &http3.Server{
+		TLSConfig: config,
+		Handler:   handler,
+	}
+
+	hErr := make(chan error)
+	qErr := make(chan error)
+
+	if !http3Only {
+		go func() {
+			hErr <- http.ListenAndServeTLS(addr, certFile, keyFile, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				quicServer.SetQuicHeaders(w.Header())
+				handler.ServeHTTP(w, r)
+			}))
+		}()
+	}
+	go func() {
+		qErr <- quicServer.Serve(udpConn)
+	}()
+
+	select {
+	case err := <-hErr:
+		quicServer.Close()
+		return err
+	case err := <-qErr:
+		// Cannot close the HTTP server or wait for requests to complete properly :/
+		return err
+	}
+}
+
+// LogHandler mix grpc and http in one Handler
+func LogHandler(h http.Handler) http.Handler {
+	mix := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if *cors {
+			enableCors(w, r)
+		}
+		req := getRequestInfo(r)
+		data, _ := json.Marshal(req)
+		log.Info().Msg(string(data))
+		h.ServeHTTP(w, r)
+	})
+	server := &http2.Server{}
+	return h2c.NewHandler(mix, server)
 }
